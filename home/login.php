8f327dfcec8a50a2dcdbb879eb0a5a4aa53787c2
ngoalongkt <ngoalongkt@ngoalongkt-PC>
2013-08-01 11:00:16 +0700

    db

--- home/login.php
+++ home/login.php
@@ -1,21 +1,28 @@
 <?php
 
+$all_define = get_defined_constants(true);
+foreach ($all_define['user'] as $key => $lang) {
+    $smarty->assign($key, $lang);
+}
 $smarty->assign('action_login', get_href_link(PAGE_LOGIN, 'action=login'));
 if ($_POST['action'] == 'process') {
     $account_number = db_prepare_input($_POST['account_number']);
     $login_password = db_prepare_input($_POST['password']);
     $security_code = db_prepare_input($_POST['security_code']);
 
-    if ($security_code == $secure_image_hash_string) {
-        $validator->validateGeneral('Account Number', $account_number, _ERROR_FIELD_EMPTY);
-        $validator->validateGeneral('Password', $login_password, _ERROR_FIELD_EMPTY);
-    } else {
-        $validator->addError('Turing Number', ERROR_SECURE_CODE_WRONG);
+    if (!empty($error_log_login) && $error_log_login > 3) {
+        if ($security_code != $secure_image_hash_string)
+            $validator->addError('Turing Number', ERROR_SECURE_CODE_WRONG);
     }
+    
+    $smarty->assign('error_log_login',$error_log_login);
+
+    $validator->validateGeneral('Account Number', $account_number, _ERROR_FIELD_EMPTY);
+    $validator->validateGeneral('Password', $login_password, _ERROR_FIELD_EMPTY);
 
     if (count($validator->errors) == 0) {
 
-        $sql_user = "SELECT user_id,  email,password FROM " . _TABLE_USERS . " WHERE account_number='" . $account_number . "'";
+        $sql_user = "SELECT user_id,  email,password,verification_status,verification_ip,verification_key,firstname,lastname FROM " . _TABLE_USERS . " WHERE account_number='" . $account_number . "'";
         $user_query = db_query($sql_user);
         if (db_num_rows($user_query) > 0) { // email passed
             // check password
@@ -33,7 +40,7 @@ if ($_POST['action'] == 'process') {
                 tep_session_register('login_userid');
                 tep_session_register('login_account_number');
                 tep_session_register('login_useremail');
-
+               
                 // set cookies for autologin
 
                 if ($_POST['remember_me']) {
@@ -43,13 +50,37 @@ if ($_POST['action'] == 'process') {
                 }
 
 
+                $current_ip = get_client_ip();
+                $mss_flag = false;
+                if (($user_info['verification_status'] == 1) && ($current_ip != $user_info['verification_ip'])) {
+                    $mss_flag = true;
+                }
+
+                if ($mss_flag) {
+                    $verification_key = tep_create_random_value(10, 'digits');
+
+                    $signup_data_array['verification_key'] = $verification_key;
+                    db_perform(_TABLE_USERS, $signup_data_array, 'update', " user_id='" . $user_info['user_id'] . "' ");
+
+
+
+                    $email_info = get_email_template('VERIFYCATION_KEY');
+
+                    $msg_subject = $email_info['emailtemplate_subject'];
+                    $msg_content = str_replace(array('[firstname]', '[verification_key]'), array($user_info['firstname'], $verification_key), $email_info['emailtemplate_content']);
+                    $msg_content = html_entity_decode($msg_content);
+                    tep_mail($user_info['firstname'] . ' ' . $user_info['lastname'], $user_info['email'], $msg_subject, $msg_content, SITE_NAME, SITE_CONTACT_EMAIL);
+                }
+
+
+
                 if (sizeof($navigation->snapshot) > 0) {
 
                     $origin_href = get_href_link($navigation->snapshot['page'], tep_array_to_string($navigation->snapshot['get'], array(tep_session_name())), $navigation->snapshot['mode']);
                     $navigation->clear_snapshot();
-                    tep_redirect($origin_href);
+                    tep_redirect($origin_href,'','SSL');
                 } else {
-                    tep_redirect(get_href_link(PAGE_LOGIN_CONFIRM));
+                    tep_redirect(get_href_link(PAGE_LOGIN_CONFIRM,'','SSL'));
                 }
             }
         } else {
@@ -60,6 +91,14 @@ if ($_POST['action'] == 'process') {
         } else {
             postAssign($smarty);
             $smarty->assign('validerrors', $validator->errors);
+        }
+    } else {
+        if (empty($error_log_login)) {
+            $error_log_login = 1;
+            tep_session_register('error_log_login');
+        } else {
+            $error_log_login++;
+            tep_session_register('error_log_login');
         }
     }
 }
--- templates_c/%%AD^AD4^AD4FF75F%%login_confirm.html.php
+++ templates_c/%%AD^AD4^AD4FF75F%%login_confirm.html.php
@@ -1,52 +1,76 @@
-<?php /* Smarty version 2.6.18, created on 2012-03-29 05:37:45
+<?php /* Smarty version 2.6.18, created on 2013-08-01 05:52:21
          compiled from home/login_confirm.html */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('function', 'dev_get_page_link', 'home/login_confirm.html', 2, false),)), $this); ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <form name="frmLoginConfirm" action="<?php echo smarty_function_dev_get_page_link(array('page' => 'PAGE_LOGIN_CONFIRM','ssl' => true), $this);?>
 " method="post" >
-<input name="action" value="process" type="hidden" />
-<div align="right">
-  <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="650" id="AutoNumber1">
-    <tr>
-      <td width="100%" height="20">&nbsp;</td>
-    </tr>
-  </table>
-</div>
-<div align="center">
-  <center>
-<table cellpadding="2" cellspacing="0" border="0" width="580" style="border-collapse: collapse" bordercolor="#111111" >
-	<tr>
-	  <td  class="pageHeading" height="53" valign="top">
-      <font color="#6666FF" face="Tahoma"><b>Login: Step 2</b></font></td>
-	</tr>
-</table>
-  </center>
-</div>
-<br />
-<div align="center">
-  <center>
-<table cellpadding="0" cellspacing="0" border="0" class="form_content" width="500" style="border-collapse: collapse" bordercolor="#111111">
-<tr><td valign="top" width="281">
+    <input name="action" value="process" type="hidden" />
+    <div align="right">
+        <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="650" id="AutoNumber1">
+            <tr>
+                <td width="100%" height="20">&nbsp;</td>
+            </tr>
+        </table>
+    </div>
+    <div align="center">
+        <center>
+            <table cellpadding="2" cellspacing="0" border="0" width="580" style="border-collapse: collapse" bordercolor="#111111" >
+                <tr>
+                    <td  class="pageHeading" height="53" valign="top">
+                        <font color="#6666FF" face="Tahoma"><b>Login: Step 2</b></font></td>
+                </tr>
+            </table>
+        </center>
+    </div>
+    <br />
+    <div align="center">
+        <center>
+            <table cellpadding="0" cellspacing="0" border="0" class="form_content" width="500" style="border-collapse: collapse" bordercolor="#111111">
+                <tr><td valign="top" width="281">
 
-	<table cellpadding="0" cellspacing="0" border="0"   width="100%" height="55" >
-		<tr><td height="28" valign="top"><font size="2" face="Tahoma">Your personal welcome message is:</font></td> </tr>			
-		<tr><td height="27"><font face="Tahoma"><b><span class="contentTitle3"><?php echo $this->_tpl_vars['personal_welcome_message']; ?>
+                        <table cellpadding="0" cellspacing="0" border="0"   width="100%" height="55" >
+                            <tr><td height="28" valign="top"><font size="2" face="Tahoma">Your personal welcome message is:</font></td> </tr>			
+                            <tr><td height="27"><font face="Tahoma"><b><span class="contentTitle3"><?php echo $this->_tpl_vars['personal_welcome_message']; ?>
 </span></b></font></td> </tr>			
-	</table>
-</td><td valign="middle" align="center" width="219" >
-	<table cellpadding="0" cellspacing="0" border="0"  width="100%" >
-		<tr><td><font size="2" face="Tahoma">Only Global Cash knows your custom welcome message. Fake web sites that want you to enter your login information on their fake login pages will not be able to show it.
-<br /><br />
-Close your browser and scan your computer for viruses if you do not see your custom welcome message during the login process.</font></td></tr>
-	</table>	
-</td></tr>
-</table>
-</center>
-  </div>
-<br />
-<table cellpadding="2" cellspacing="2" border="0" width="100%" height="98" >
-<tr ><td align="center"  class="contenButtons" height="92" valign="top"> 
-  <font face="Tahoma"> <input  name="confirm_message" id="confirm_message" type="checkbox"    value="1"  onchange="checkConfirm();" /></font><font size="2" face="Tahoma"> I confirm that my custom welcome message is correct &nbsp;</font><input  type="submit" id="buttonContinue" class="button"  value="Continue"  disabled="disabled" /></td></tr>
-</table>
+                        </table>
+                    </td><td valign="middle" align="center" width="219" >
+                        <table cellpadding="0" cellspacing="0" border="0"  width="100%" >
+                            <tr><td><font size="2" face="Tahoma">Only Global Cash knows your custom welcome message. Fake web sites that want you to enter your login information on their fake login pages will not be able to show it.
+                                    <br /><br />
+                                    Close your browser and scan your computer for viruses if you do not see your custom welcome message during the login process.</font></td></tr>
+                        </table>	
+                    </td></tr>
+            </table>
+        </center>
+    </div>
+    <br />
+    <table cellpadding="2" cellspacing="2" border="0" width="100%" height="98" >
+        <tr >
+            <td width="100">&nbsp;</td>
+            <td align="left"  class="contenButtons" valign="top"> 
+                <font face="Tahoma"> 
+                <input  name="confirm_message" id="confirm_message" type="checkbox"    value="1"  onchange="checkConfirm();" /></font>
+                <font size="2" face="Tahoma"> I confirm that my custom welcome message is correct &nbsp;</font>
+                
+            </td>
+            
+        </tr>
+         <?php if (! empty ( $this->_tpl_vars['mss_flag'] )): ?>
+        <tr>
+             <td>&nbsp;</td>
+            <td align="left"  class="contenButtons" valign="top"> 
+                <font size="2" face="Tahoma">Please check your email to get verification code&nbsp;</font>
+                <input type="text" value="" id="verification_key" autocomplete="off" class="st-forminput medium" size="20" name="verification_key">
+            </td>
+        </tr>
+        <?php endif; ?>
+        
+        <tr>
+             <td>&nbsp;</td>
+            <td align="left"  class="contenButtons" valign="top"> 
+                <input  type="submit" id="buttonContinue" class="button"  value="Continue"  disabled="disabled" />
+            </td>
+        </tr>
+    </table>
 </form>
